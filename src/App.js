39f07d91afadd24b6ba42602811471eef0b17d96
Hanwen Cheng <heawen.cheng@gmail.com>
2017-06-09 22:08:10 +0200

    add canvas component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router';
 import logo from './logo.svg';
+import Canvas from './components/Canvas';
 import './App.css';
+import Loader from './core/loader';
 
-const App = ({name}) => (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to SanPlay</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-);
+const App = ({name}) => {
+
+    const init = () => {
+        console.log('loader is', Loader)
+    };
+
+    init();
+
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to SanPlay</h2>
+            </div>
+            <Canvas/>
+            <p className="App-intro">
+            </p>
+        </div>
+    )
+}
 
 export default App;
