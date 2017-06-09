f58848efd2d53c4a90f02a172c716e424c0cd355
Hanwen Cheng <heawen.cheng@gmail.com>
2017-06-09 22:08:21 +0200

    add keyboard and loader

---
+++ src/core/loader.js
@@ -0,0 +1,28 @@
+
+var Loader = {
+    images: {}
+};
+
+Loader.loadImage = function (key, src) {
+    var img = new Image();
+
+    var d = new Promise(function (resolve, reject) {
+        img.onload = function () {
+            this.images[key] = img;
+            resolve(img);
+        }.bind(this);
+
+        img.onerror = function () {
+            reject('Could not load image: ' + src);
+        };
+    }.bind(this));
+
+    img.src = src;
+    return d;
+};
+
+Loader.getImage = function (key) {
+    return (key in this.images) ? this.images[key] : null;
+};
+
+export default Loader;
